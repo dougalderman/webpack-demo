6fcc11dc07d7c39eee799c3e595124012efee17c
Doug Alderman <agnadcp-d1@yahoo.com>
2018-03-09 16:10:55 -0700

    Added sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,12 +10,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.(s*)css$/,
+        /*use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        }) */
+        use:['style-loader','css-loader', 'sass-loader']
       }
     ]
-  }
+  },
+  /* plugins: [
+    new ExtractTextPlugin('styles.css')
+    //if you want to pass in options, you can do so:
+    //new ExtractTextPlugin({
+    //  filename: 'style.css'
+    //})
+  ] */
 };
